db0cc1ad0afa7fd0f3ce32b5e6058e0af3c29a16
Nightwalkx <74856680+xi-jiajun@users.noreply.github.com>
2023-03-29 22:59:53 -0400

    update allsides dataset

---
+++ model/load_allsides_dataset.py
@@ -0,0 +1,40 @@
+from torch.utils.data import Dataset, DataLoader
+from transformers import RobertaTokenizer
+import torch
+from transformers import RobertaModel
+import torch
+import json
+
+
+class allsidesData(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+
+def main():
+    with open('./data/allsides.jsonl', 'r') as fp:
+        input = json.loads(fp.read())
+
+    BATCH_SIZE = 64
+
+    allsides = allsidesData(input)
+    train_params = {
+        'batch_size': BATCH_SIZE,
+        'shuffle': True,
+        'num_workers': 0
+    }
+    trainloader = DataLoader(allsides, **train_params)
+    for _, data in enumerate(trainloader, 0):
+        print(data['content'][0])
+        print(data['bias'].shape)
+        break
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/load_wiki_dataset.py
@@ -0,0 +1,37 @@
+from torch.utils.data import Dataset, DataLoader
+from transformers import RobertaTokenizer
+import torch
+from transformers import RobertaModel
+
+
+class wikiData(Dataset):
+    def __init__(self, data):
+        self.data = data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+
+def main():
+    with open('./data/wiki1m_for_simcse.txt', 'r', encoding='UTF-8') as f:
+        input_text = f.readlines()
+
+    # MAX_LEN = 256  # RoBerta model accept 512 at most.
+    BATCH_SIZE = 64
+
+    # tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+    
+    wiki = wikiData(input_text)
+    train_params = {
+        'batch_size': BATCH_SIZE,
+        'shuffle': True,
+        'num_workers': 0
+    }
+    trainloader = DataLoader(wiki, **train_params)
+
+
+if __name__ == "__main__":
+    main()
