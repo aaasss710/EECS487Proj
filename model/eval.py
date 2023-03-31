39345637774f96dfebd9b98d8941eee758582a41
Nightwalkx <74856680+xi-jiajun@users.noreply.github.com>
2023-03-31 15:13:06 -0400

    update eval

---
+++ model/eval.py
@@ -0,0 +1,67 @@
+import sys
+import torch
+from transformers import RobertaTokenizer
+from RoBERTa import CustomRobertaModel
+# Set path to SentEval
+PATH_TO_SENTEVAL = '../SentEval'
+PATH_TO_DATA = '../SentEval/data'
+
+# Import SentEval
+sys.path.insert(0, PATH_TO_SENTEVAL)
+import senteval
+
+def evaluate(model, tokenizer, device):
+    
+    def prepare(params, samples):
+        return
+    
+    def batcher(params, batch):
+        sentences = [' '.join(s) for s in batch]
+        batch = tokenizer.batch_encode_plus(
+            sentences,
+            return_tensors='pt',
+            padding=True,
+        )
+        for k in batch:
+            batch[k] = batch[k].to(device)
+        with torch.no_grad():
+            outputs = model(**batch, output_hidden_states=True, return_dict=True, sent_emb=True)
+            pooler_output = outputs.pooler_output
+        return pooler_output.cpu()
+    
+    # Set params for SentEval (fastmode)
+    params = {'task_path': PATH_TO_DATA, 'usepytorch': True, 'kfold': 5}
+    params['classifier'] = {'nhid': 0, 'optim': 'rmsprop', 'batch_size': 128,
+                            'tenacity': 3, 'epoch_size': 2}
+
+    se = senteval.engine.SE(params, batcher, prepare)
+    
+    
+    tasks = ['STSBenchmark', 'SICKRelatedness', 'MR',
+                'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']
+    model.eval()
+    results = se.eval(tasks)
+
+    stsb_spearman = results['STSBenchmark']['dev']['spearman'][0]
+    sickr_spearman = results['SICKRelatedness']['dev']['spearman'][0]
+
+    metrics = {"eval_stsb_spearman": stsb_spearman, "eval_sickr_spearman": sickr_spearman,
+                "eval_avg_sts": (stsb_spearman + sickr_spearman) / 2}
+
+    avg_transfer = 0
+    for task in ['MR', 'CR', 'SUBJ', 'MPQA', 'SST2', 'TREC', 'MRPC']:
+        avg_transfer += results[task]['devacc']
+        metrics['eval_{}'.format(task)] = results[task]['devacc']
+    avg_transfer /= 7
+    metrics['eval_avg_transfer'] = avg_transfer
+
+    return metrics
+
+
+def main():
+    tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+    model = CustomRobertaModel()
+    evaluate(model, tokenizer, 'cuda')
+
+if __name__ == "__main__":
+    main()
